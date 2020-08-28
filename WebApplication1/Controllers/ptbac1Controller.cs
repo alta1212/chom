cee7244a3956e654c772a8d6d4ddc14585291c15
alta1212 <doremon209a@gmail.com>
2020-08-28 10:17:04 +0700

    Add project files.

---
+++ WebApplication1/Controllers/ptbac1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ptbac1Controller : Controller
+    {
+        // GET: ptbac1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult tinh(string so_1,string so_2)
+        {
+            if(so_1!=null &&so_2!=null)
+            {   
+                float  a = float .Parse(so_1);
+                float  b = float .Parse(so_2);
+                float  x = -b / a;      
+                ViewBag.kq = x.ToString();
+            }    
+            return View("tinh");
+
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ptbac2Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ptbac2Controller : Controller
+    {
+        // GET: ptbac2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ptbac2(string so1,string so2,string so3)
+        {
+            double a=0, b=0, c=0;
+            if (so1!=null && so2 != null && so3 != null )
+            {
+              a = double.Parse(so1);
+              b = double.Parse(so2);
+              c = double.Parse(so3);
+            }    
+           
+            double x1, x2;
+            double   d  = b * b - 4 * a * c;
+            if (d == 0)
+            {
+               
+                x1 = -b / (2.0 * a);
+                x2 = x1;
+                ViewBag.kq = x1.ToString();
+            }
+            else if (d > 0)
+            {
+                Console.Write("Phuong trinh co hai nghiem thuc phan biet\n");
+
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+                ViewBag.kq = string.Format("Nghiệm 1 {0} nghiệm 2 {1}",x1,x2);
+            }
+            else
+              ViewBag.kq=("Phuong trinh vo nghiem (hay khong co nghiem thuc nao)\n\n");
+            return View("tinh2");
+        }    
+    }
+}
